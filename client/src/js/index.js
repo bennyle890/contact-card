df3b5232669c4a7a31287615d5280fc97b5b6391
Benny <benny.le890@gmail.com>
2022-11-22 13:35:36 -0500

    successful?

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,16 +10,22 @@ import "../css/index.css";
 import { Tooltop, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Import initDb
-import { initdb } from './database';
-
 // Import Images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+// Import initDb
+import { initdb, getDb, postDb } from './database';
+
 window.addEventListener('load', function() {
-    initdb();
+    initdb()
+
+    // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
+    getDb();
+    postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+    getDb();
+    
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
