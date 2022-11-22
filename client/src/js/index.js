254ff1945b83fe6384fb8665b97a7d2f3ecb01b5
Benny <benny.le890@gmail.com>
2022-11-22 13:16:02 -0500

    finished contacts

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,7 @@
 // Import Modules
 import "./form";
 import "./submit";
+// import "./database";
 
 // Import CSS files
 import "../css/index.css";
@@ -9,12 +10,16 @@ import "../css/index.css";
 import { Tooltop, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Import initDb
+import { initdb } from './database';
+
 // Import Images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function() {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
